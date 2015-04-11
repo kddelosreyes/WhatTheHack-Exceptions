00265cd9b8c0d1de022d06606b95a537ca49f3bb
Stephen Ong <sdong@rogomi.com>
2015-04-12 00:02:43 +0800

    fghj

--- WhatTheHack/src/com/feutech/whatthehack/fragments/MapFragment.java
+++ WhatTheHack/src/com/feutech/whatthehack/fragments/MapFragment.java
@@ -1,9 +1,6 @@
 package com.feutech.whatthehack.fragments;
 
-import com.feutech.whatthehack.R;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.SupportMapFragment;
-
+import GPS.GPSTracker;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -11,11 +8,21 @@ import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.feutech.whatthehack.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapFragment extends Fragment{
 
 	private SupportMapFragment mapFragment;
 	private GoogleMap map;
+	private GPSTracker gps;
 	
 	private View view;
 	
@@ -47,5 +54,23 @@ public class MapFragment extends Fragment{
 		
 		if (map == null)
 			map = mapFragment.getMap();
+		
+		gps = new GPSTracker(getActivity());
+		if(gps.canGetLocation()){
+			double latitude;
+        	double longitude;
+			
+        	latitude = gps.getLatitude();
+            longitude = gps.getLongitude();
+            
+            map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+            map.getUiSettings().setZoomControlsEnabled(true);
+            CameraPosition cameraPosition = new CameraPosition.Builder().target(
+                    new LatLng(latitude, longitude)).zoom(16).build();
+            map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+            // create marker
+            MarkerOptions marker = new MarkerOptions().position(new LatLng(latitude, longitude)).title("Im here");
+            map.addMarker(marker);
+		}
 	}
 }
