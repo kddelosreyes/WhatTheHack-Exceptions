ca463b54f2b606098ac2014c04e5d5be7d514c9c
sonymikhael <sonymikhael07@gmail.com>
2015-04-11 20:29:47 +0800

    commit to fix github errors

--- WhatTheHack/src/com/feutech/whatthehack/PostStatusActivity.java
+++ WhatTheHack/src/com/feutech/whatthehack/PostStatusActivity.java
@@ -33,37 +33,3 @@ public class PostStatusActivity extends Activity {
 		return super.onOptionsItemSelected(item);
 	}
 }
-package com.feutech.whatthehack;
-
-import android.app.Activity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-
-public class PostStatusActivity extends Activity {
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_post_status);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.post_status, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
-}
