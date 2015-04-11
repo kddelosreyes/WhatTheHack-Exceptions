dd4a219bb2009247428ddd89265e0e7a45f36b52
sonymikhael <sonymikhael07@gmail.com>
2015-04-11 20:06:45 +0800

    camera launched from places activity

--- WhatTheHack/src/com/feutech/whatthehack/PostStatusActivity.java
+++ WhatTheHack/src/com/feutech/whatthehack/PostStatusActivity.java
@@ -7,6 +7,7 @@ import android.view.MenuItem;
 
 public class PostStatusActivity extends Activity {
 
+	public static final String PostStatusActivity_PhotoPath = "com.feutech.whatthehack.PostStatusActivity.PhotoPath";
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
