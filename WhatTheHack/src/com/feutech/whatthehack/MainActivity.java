12bdc36047f01b45b8361957d023a7661789b5fb
Kim Howel <dr.kimhowel@gmail.com>
2015-04-11 12:52:31 +0800

    Action Bar Layout

--- WhatTheHack/src/com/feutech/whatthehack/MainActivity.java
+++ WhatTheHack/src/com/feutech/whatthehack/MainActivity.java
@@ -7,8 +7,6 @@ import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
 import android.view.View.OnClickListener;
